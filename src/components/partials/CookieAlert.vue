6cdbd706c3df3cfc2b9252b389133ad08e8da6ff
Richard Zille <therichkid@gmx.de>
2020-11-22 21:10:44 +0100

    Change cookie alert design

--- src/components/partials/CookieAlert.vue
+++ src/components/partials/CookieAlert.vue
@@ -1,36 +1,28 @@
 <template>
-  <v-bottom-sheet v-model="hasNotAcceptedCookies" persistent hide-overlay>
-    <v-sheet
-      :color="$vuetify.theme.dark ? '#666666' : '#e4e4e4'"
-      class="text-center"
-      style="border-bottom-left-radius: 0; border-bottom-right-radius: 0"
-    >
-      <v-container>
-        <v-row dense justify="center">
-          <v-col cols="12">
-            <span>
-              Mit der Nutzung dieser Webseite erklären Sie sich damit einverstanden, dass wir
-              Cookies verwenden.
-            </span>
-            <v-btn text to="/datenschutz">Weiterlesen</v-btn>
-          </v-col>
-          <v-col cols="auto" class="mr-1">
-            <v-checkbox
-              v-model="hasUnderstood"
-              label="Ich habe verstanden"
-              hide-details
-              class="my-0"
-            ></v-checkbox>
-          </v-col>
-          <v-col cols="auto" class="ml-1">
-            <v-btn :disabled="!hasUnderstood" @click="acceptCookies()" color="success"
-              >Akzeptieren
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-sheet>
-  </v-bottom-sheet>
+  <div class="cookie-consent pa-2 pa-md-5" v-if="hasNotAcceptedCookies">
+    <v-card outlined style="max-width: 600px">
+      <v-card-text class="pb-0">
+        <span>
+          Mit der Nutzung dieser Webseite erklären Sie sich damit einverstanden, dass wir Cookies
+          verwenden.
+        </span>
+        <v-btn text to="/datenschutz">Weiterlesen</v-btn>
+      </v-card-text>
+      <v-card-actions>
+        <v-checkbox
+          v-model="hasUnderstood"
+          label="Ich habe verstanden"
+          hide-details
+          color="success"
+          class="mt-0 pt-0"
+        ></v-checkbox>
+        <v-spacer></v-spacer>
+        <v-btn :disabled="!hasUnderstood" @click="acceptCookies()" color="success"
+          >Akzeptieren
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
 
 <script>
@@ -60,4 +52,13 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.cookie-consent {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  z-index: 5;
+}
+</style>
